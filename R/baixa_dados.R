df948b0140c8caa455314e6ea8aa9d16fcd1e993
João Victor Batista Lopes <batistajvictor@gmail.com>
2025-12-09 16:51:46 -0300

    feat/adiciona graficos

--- R/baixa_dados.R
+++ R/baixa_dados.R
@@ -1,13 +1,13 @@
-baixa_dados <- function(output = "data/clean_data"){
+baixa_dados <- function(year = 2024, output = "data/clean_data"){
   
   # Carregar pacotes necessários
   if (!require("PNADcIBGE")) stop("Pacote PNADcIBGE não instalado. Use: install.packages('PNADcIBGE')")
   if (!require("readr")) stop("Pacote readr não instalado. Use: install.packages('readr')")
   
-  message("Baixando dados da PNADC 2024 - Trimestre 4")
-  dados <- PNADcIBGE::get_pnadc(year = 2024, topic = 4)
+  message(sprintf("Baixando dados da PNADC %d - Entrevista 1", year))
+  dados <- PNADcIBGE::get_pnadc(year, interview = 1)
   
-  saveRDS(dados, file = file.path(output, "pnadc_design.RDS"))
+  saveRDS(dados, file = file.path(output, sprintf("pnadc_design_%d.RDS", year)))
   
-  message("Dados salvos em:", file.path(output, "pnadc_design.RDS"))
+  message("Dados salvos em:", file.path(output, sprintf("pnadc_design_%d.RDS", year)))
 }
